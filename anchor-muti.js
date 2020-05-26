3987a6f798a37e7a47874b887c855c29a2f12e3d
necho.duan <necho.duan@sunvalley.com.cn>
2020-05-26 18:11:31 +0800

    ip地址

--- anchor-muti.js
+++ anchor-muti.js
@@ -22,11 +22,39 @@ let uid = argumentsx[1];
  {
   const page = await browser.newPage();
   await page.goto('https://47.106.74.130:8441/webRtc.html');
-  await page.screenshot({path: 'example.png'});
+  // await page.screenshot({path: 'example.png'});
   await page.type('input[id=userName]','xiaopang'+i);
-  await page.type('input[id=roomName]',roomName+i);
+  await page.type('input[id=roomName]',roomName+'-'+getLocalIP()+'-'+i);
   await page.click('#type1');
   await page.click('#createRoom');
-  await page.screenshot({path: 'example2.png'});
+  // await page.screenshot({path: 'example2.png'});
  }	 
 })();
+
+
+
+function getLocalIP() {
+    const os = require('os');
+    const osType = os.type(); //系统类型
+    const netInfo = os.networkInterfaces(); //网络信息
+    let ip = '';
+    if (osType === 'Windows_NT') { 
+        for (let dev in netInfo) {
+        	//win7的网络信息中显示为本地连接，win10显示为以太网
+            if (dev === '本地连接' || dev === '以太网') {
+                for (let j = 0; j < netInfo[dev].length; j++) {
+                    if (netInfo[dev][j].family === 'IPv4') {
+                        ip = netInfo[dev][j].address;
+                        break;
+                    }
+                }
+            }
+        }
+
+    } else if (osType === 'Linux') {
+        ip = netInfo.eth0[0].address;
+    }
+
+    return ip;
+}
+

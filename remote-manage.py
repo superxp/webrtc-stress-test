edd5e326839716ace0b4de08a1646e2baa26dd6a
necho.duan <necho.duan@sunvalley.com.cn>
2020-05-28 16:28:25 +0800

    ttt

--- remote-manage.py
+++ remote-manage.py
@@ -4,7 +4,7 @@ import sys
 import os
 import subprocess
 
-#python remote-manage.py -anchor 10 -audience 10 -roomName 996 -t 100 
+#python remote-manage.py -anchor 10 -audience 50 -roomName 996 -t 100 
 # 
 # -anchor 主播数量
 # -audience 观众数量
@@ -20,30 +20,46 @@ if len(args) < 9 :
 	print 'miss 缺少参数'
 	sys.exit(-1)
 
+
+#需要分配的主播数
 anchorNum = args[2]
+#需要分配的观众数
 audienceNum = args[4]
 roomName = args[6]
 
 anchorMachineIp={"durant":"10.20.12.237","durantWindows1":"10.20.10.53"}
-anchorRoleList = ['durant','durantWindows1']
+
+#主播2台机器
+anchorRoleList = ['durant']
+#观众端1台机器
 audienceRoleList = ['durant']
 
 
 
-
+#每台机器分配的主播房间数
 everyMachineAnchorRoomNum = int(anchorNum)/len(anchorRoleList)
-mxx = int(anchorNum)%len(anchorRoleList)
 
 
-everyMachineAudienceRoomNum = int(audienceNum)/len(audienceRoleList)
 
+#每台机器分配的观众数
+everyMachineAudienceNum  = int(audienceNum)/len(audienceRoleList)
+
+#每个房间的观众数
+everyRoomAudience = int(audienceNum)/ int(anchorNum);
+
+
+
+print everyRoomAudience
 
 for i in anchorRoleList:
-	subprocess.Popen('python remote-exec.py -h '+i+' -role anchor  -n '+str(everyMachineAnchorRoomNum)+' -t 100 -roomName '+roomName);
+	os.system('python remote-exec.py -h '+i+' -role anchor  -n '+str(everyMachineAnchorRoomNum)+' -t 100 -roomName '+roomName);
 	
-
+	
+	
 for i in audienceRoleList:
-	subprocess.Popen('python remote-exec.py -h '+i+' -role audience  -n '+str(everyMachineAudienceRoomNum)+' -t 100 -roomName '+roomName+'-'+anchorMachineIp[i]);	
+	for x in anchorRoleList:
+		for y in range (everyMachineAnchorRoomNum):
+			os.system('python remote-exec.py -h '+i+' -role audience  -n '+str(everyRoomAudience)+' -t 100 -roomName '+roomName+'-'+anchorMachineIp[x]+"-"+str(y));
 	
 	
  
